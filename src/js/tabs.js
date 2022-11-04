a0c1212a6c5843d86b4d55270e8ba9120a6f330a
arichild <arieevq@gmail.com>
2022-11-04 20:12:10 +0300

    save

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,12 +1,12 @@
-let openTabs = document.querySelector('.ui-breadcrumbs.tabs');
+let openTabs = document.querySelector('.ui-tab.tabs');
 
 openTabs.addEventListener('click', (e) => {
-  if(e.target.classList[0] === 'ui-breadcrumbs-link') {
+  if(e.target.classList[0] === 'ui-tab-link') {
     let dataName = e.target.dataset.name;
     let activeTab = document.querySelector('.swiper-wrapper.active');
-    let activeLink = document.querySelector('.ui-breadcrumbs-link.active');
+    let activeLink = document.querySelector('.ui-tab-link.active');
     let tab = document.querySelector(`.swiper-wrapper.${dataName}`);
-    let tabLink = document.querySelector(`.ui-breadcrumbs-link.${dataName}`);
+    let tabLink = document.querySelector(`.ui-tab-link.${dataName}`);
 
     activeTab.classList.remove('active');
     activeLink.classList.remove('active');
