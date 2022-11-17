d8e388577d3c577370a4377d11a9bcf5ecb7a6a8
arichild <arieevq@gmail.com>
2022-11-18 00:44:28 +0300

    save

--- src/js/validation.js
+++ src/js/validation.js
@@ -29,6 +29,24 @@ $(".order-form").validate({
     address: {
       required: true,
     },
+    select1: {
+      required: true,
+    },
+    select: {
+      required: true,
+    }
+  },
+
+  errorPlacement: function (error, element) {
+    console.log(error)
+    if (element.hasClass('ui-radio')) {
+      console.log("asdasd")
+      element.closest('.ui-select').after(error);
+    }
+    if (element.hasClass('ui-input')) {
+      console.log("asdasd")
+      element.closest('.ui-input').after(error);
+    }
   },
 
   messages: {
@@ -58,5 +76,15 @@ $(".order-form").validate({
     address: {
       required: "Пожалуйста, введите данные",
     },
+    select1: {
+      required: "Пожалуйста, выберете способ доставки",
+    },
+    select: {
+      required: "Пожалуйста, выберете способ оплаты",
+    },
   }
 });
+
+jQuery.validator.addMethod("lettersonly", function(value, element) {
+  return this.optional(element) || /^[a-z]+$/i.test(value);
+}, "Letters only please");
