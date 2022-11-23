3c9b8877a9985421e319c766c173ab00f44e8e20
arichild <arieevq@gmail.com>
2022-11-23 20:19:50 +0300

    save

--- src/js/input-search.js
+++ src/js/input-search.js
@@ -2,14 +2,16 @@ const search = document.getElementById('search');
 const btnClear = document.querySelector('.header-control-btn.clear-input');
 const searchResult = document.querySelector('.header-search-result');
 
-search.addEventListener('focus', () => {
+search.addEventListener('focus', (e) => {
   searchResult.classList.add('active')
   btnClear.classList.add('active')
 });
 
-// search.addEventListener('blur', () => {
-//   searchResult.classList.remove('active')
-// });
+document.body.addEventListener('click', (e) => {
+  if(e.target.closest('.header-search-result.active') === null && e.target !== search && e.target !== btnClear) {
+    searchResult.classList.remove('active')
+  }
+});
 
 btnClear.addEventListener('click', (e) => {
   const input = document.querySelector('.ui-input');
