4205a9ef1fca73f0941780f567e76abd36e8c7a7
arichild <arieevq@gmail.com>
2023-03-20 16:45:39 +0300

    Save

--- src/js/index.js
+++ src/js/index.js
@@ -25,62 +25,55 @@ function labelForInput(selector) {
   }
 }
 
-function lettersOnly() {
-  jQuery.validator.addMethod("lettersonly", function(value, element) {
-    return this.optional(element) || /^([а-яё ]+|[a-z ]+)$/i.test(value);
-  }, "Поле может состоять из букв и пробелов, без цифр");
-}
+jQuery.validator.addMethod("lettersonly", function(value, element) {
+  return this.optional(element) || /^([а-яё ]+|[a-z ]+)$/i.test(value);
+}, "Поле может состоять из букв и пробелов, без цифр");
 
-function phoneRule() {
-  if (document.getElementById('phone')) {
-    let phone = document.getElementById('phone')
+if (document.getElementById('phone')) {
+  let phone = document.getElementById('phone')
 
-    let phoneMask = IMask(phone, {
-      mask: [
-        {
-          mask: '+{375} (00) 000 00 00',
-          startsWith: '375',
-          overwrite: true,
-          lazy: false,
-          placeholderChar: '_',
-        },
-        {
-          mask: '+{7} (000) 000 00 00',
-          startsWith: '7',
-          overwrite: true,
-          lazy: false,
-          placeholderChar: '_',
-        },
-        {
-          mask: '0000000000000',
-          startsWith: '',
-          country: 'unknown'
-        }
-      ],
-
-      dispatch: function (appended, dynamicMasked) {
-        var number = (dynamicMasked.value + appended).replace(/\D/g, '');
-
-        return dynamicMasked.compiledMasks.find(function (m) {
-          return number.indexOf(m.startsWith) === 0;
-        });
+  let phoneMask = IMask(phone, {
+    mask: [
+      {
+        mask: '+{375} (00) 000 00 00',
+        startsWith: '375',
+        overwrite: true,
+        lazy: false,
+        placeholderChar: '_',
+      },
+      {
+        mask: '+{7} (000) 000 00 00',
+        startsWith: '7',
+        overwrite: true,
+        lazy: false,
+        placeholderChar: '_',
+      },
+      {
+        mask: '0000000000000',
+        startsWith: '',
+        country: 'unknown'
       }
-    })
-  }
+    ],
 
-  jQuery.validator.addMethod("phone", function(value, element) {
-    if(value.startsWith('+375')) {
-      return /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){12}(\s*)?$/i.test(value);
-    } else if(value.startsWith('+7')) {
-      return /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){11}(\s*)?$/i.test(value);
-    } else {
-      return /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){11,14}(\s*)?$/i.test(value);
+    dispatch: function (appended, dynamicMasked) {
+      var number = (dynamicMasked.value + appended).replace(/\D/g, '');
+
+      return dynamicMasked.compiledMasks.find(function (m) {
+        return number.indexOf(m.startsWith) === 0;
+      });
     }
-  }, "Введите полный номер");
+  })
 }
 
-lettersOnly()
-phoneRule()
+jQuery.validator.addMethod("phone", function (value, element) {
+  if (value.startsWith('+375')) {
+    return /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){12}(\s*)?$/i.test(value);
+  } else if (value.startsWith('+7')) {
+    return /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){11}(\s*)?$/i.test(value);
+  } else {
+    return /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){11,14}(\s*)?$/i.test(value);
+  }
+}, "Введите полный номер");
 
 function checkInputValue(input) {
   const inputField = document.querySelectorAll(input);
